3eb68301a5306317136fbeb7adedb0abe40107be
Muhammad Rafli Firmansyah <mrfirmansyah47@gmail.com>
2026-02-02 12:25:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('from kipel to willy').split('')
+    const titles = ('i love you syifa').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
